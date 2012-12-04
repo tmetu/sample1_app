cfc2b33d2bc184262dbd5b890cf6a5a27412a4c7
tmetu <your.tmetu@hotmail.com>
2012-12-04 03:13:17 -0600

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Sample1App::Application.routes.draw do
+
+  get "users/new"
+
+  root to: 'static_pages#home'
+  
+  match '/signup',  to: 'users#new'
+
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
+  
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  # root :to => "welcome#index"
+  
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
+  get "static_pages/contact"
  end
